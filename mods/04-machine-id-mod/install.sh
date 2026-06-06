56c62929f9990b85f3ca2e1ec20f8029bd8cbd23
Anduin Xue <anduin@aiursoft.com>
2026-06-06 13:32:27 +0000

    refactor: add installation of dbus and uuid-runtime in machine id setup

--- mods/04-machine-id-mod/install.sh
+++ mods/04-machine-id-mod/install.sh
@@ -2,6 +2,12 @@ set -e                  # exit on error
 set -o pipefail         # exit on pipeline error
 set -u                  # treat unset variable as error
 
+print_ok "Installing dbus and uuid-runtime..."
+apt install $INTERACTIVE \
+    dbus \
+    uuid-runtime
+judge "Install dbus and uuid-runtime"
+
 print_ok "Configuring machine id..."
 dbus-uuidgen > /etc/machine-id
 ln -fs /etc/machine-id /var/lib/dbus/machine-id
